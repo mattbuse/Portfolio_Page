8381a66c8c2bd9bdd90866b3ddd7265dd24ecbdd
mattbuse <118133088+mattbuse@users.noreply.github.com>
2023-01-01 00:31:59 -0600

    lots of changes

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let moreAbout = document.getElementById('more-about-text');
 let hideMoreInfo = document.getElementById('hide-again');
 
 let showMore = function(event) {
-    moreInfo.style.visibility = 'collapse';
-    moreAbout.style.visibility = 'visible';
+    moreInfo.style.display = 'none';
+    moreAbout.style.display = 'block';
 }
 
 let hideAgain = function(event) {
-    moreInfo.style.visibility = 'visible';
-    moreAbout.style.visibility = 'collapse';
+    moreInfo.style.display = 'block';
+    moreAbout.style.display = 'none';
 }
 
 moreInfo.addEventListener('click',showMore);
