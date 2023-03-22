89b14f3a9a060ff85cdb09ffacfe320addb29d2d
mattbuse <118133088+mattbuse@users.noreply.github.com>
2023-03-22 13:21:36 -0500

    adding React folder

---
+++ React Projects/my-cell-phone-store/my-cell-phone-store/src/components/product.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { ShopContext } from './shop-context';
+
+export const Product = (props) => {
+    const { id, productName, price, productImage } = props.data;
+    const {addToCart, cartItems} = useContext(ShopContext);
+    const cartItemAmount = cartItems[id];
+
+    return (
+        <div className="product">
+            <img src={productImage} alt=''/>
+            <div className="description">
+                <p>
+                    <b>{productName}</b>
+                </p>
+                <p>${price}</p>
+            </div>
+            <button className="addToCartButton" onClick={() => addToCart(id)}>Add to Cart {cartItemAmount > 0 && <>({cartItemAmount})</>}</button>
+        </div>
+    )
+}
---
+++ React Projects/my-cell-phone-store/my-cell-phone-store/src/products.js
@@ -0,0 +1,66 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.png';
+import product7 from './assets/7.png';
+import product8 from './assets/8.png';
+import product9 from './assets/9.png';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Apple iPhone 14 Pro',
+        price: 999,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: 'Google Pixel 7 Pro',
+        price: 899,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: 'Samsung Galaxy S23 Ultra',
+        price: 1199,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: 'Samsung Galaxy Z Fold 4',
+        price: 1799,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: 'Apple iPhone 14',
+        price: 799,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: 'Google Pixel 7',
+        price: 599,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: 'OnePlus 11',
+        price: 699,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: 'Motorola Razr',
+        price: 599,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: 'Samsung Galaxy Z Flip 4',
+        price: 999,
+        productImage: product9,
+    },
+]
